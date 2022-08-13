726c8e9da7a8bae5df06fe4e9a6d77924636fd35
Nitin <105773454+Nitin-0@users.noreply.github.com>
2022-08-13 09:50:41 +0530

    Create random_responses.py
    This python file stores responses of chatbot

---
+++ random_responses.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  7 11:47:39 2022
+
+@author: nitin kumar
+"""
+
+import random
+
+def random_string():
+    random_list = [
+        "Please try writing something more descriptive.",
+        "Oh! It appears you wrote something I don't understand yet",
+        "Do you mind trying to rephrase that?",
+        "I'm terribly sorry, I didn't quite catch that.",
+        "I can't answer that yet, please try asking something else."
+    ]
+
+    list_count = len(random_list)
+    random_item = random.randrange(list_count)
+
+    return random_list[random_item]
